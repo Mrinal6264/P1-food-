75ae6ddb7b033b9daad6b90fab31c037a541c967
Mrinal6264 <162491439+Mrinal6264@users.noreply.github.com>
2026-09-10 11:16:28 +0530

    Remove trailing slash from baseUrl

--- lib/config.dart
+++ lib/config.dart
@@ -7,6 +7,6 @@
 // set baseUrl to 'https://mystore.com/foodapp'
 // ============================================
 class AppConfig {
-  static const String baseUrl = 'https://pakhanjurone.in/';
+  static const String baseUrl = 'https://pakhanjurone.in';
   static String get apiUrl => '$baseUrl/api';
 }
